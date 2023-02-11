e64bbf46b08e5060a30bf1b019e0549d21ebadc3
Mateusz Zachacz <mateusz.zachacz@gmail.com>
2023-02-11 15:40:08 +0100

    All tests of InvoiceTest working

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
@@ -8,23 +9,83 @@ import pl.edu.agh.mwo.invoice.product.Product;
 public class Invoice {
     private Collection<Product> products;
 
+    public Invoice() {
+
+        this.products = new ArrayList<Product>();
+    }
+
     public void addProduct(Product product) {
-        // TODO: implement
+
+        if (product == null) {
+
+            throw new IllegalArgumentException("Product can not be null.");
+        }
+
+        this.products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+
+        if (quantity <= 0) {
+
+            throw new IllegalArgumentException("Quantity can not be negative.");
+        }
+
+        for (int i=0; i <  quantity; i++) {
+
+            this.products.add(product);
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+
+        BigDecimal subtotal = BigDecimal.ZERO;
+
+        if (products.isEmpty()) {
+
+            return subtotal;
+        }
+
+        for (Product product : products) {
+
+            subtotal = subtotal.add(product.getPrice());
+
+        }
+
+        return subtotal;
     }
 
     public BigDecimal getTax() {
-        return null;
+
+        BigDecimal taxAmount = BigDecimal.ZERO;
+
+        if (products.isEmpty()) {
+
+            return taxAmount;
+        }
+
+        for (Product product : products) {
+
+            taxAmount = taxAmount.add(product.getPrice().multiply(product.getTaxPercent()));
+        }
+
+        return taxAmount;
     }
 
     public BigDecimal getTotal() {
-        return null;
+
+        BigDecimal total = BigDecimal.ZERO;
+
+        if (products.isEmpty()) {
+
+            return total;
+        }
+
+        for (Product product : products) {
+
+            total = total.add(product.getPriceWithTax());
+        }
+
+        return total;
     }
 }
